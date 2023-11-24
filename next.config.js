83f9371d9c5f3f7b308947f1851fadb5c4ecb1b8
Eder <eferreira@titaniumindustrialsecurity.com>
2023-11-24 09:30:28 +0100

    Trying deployment

--- next.config.js
+++ next.config.js
@@ -2,7 +2,17 @@
 const nextConfig = {
   images: {
     remotePatterns: [{ protocol: 'https', hostname: '**' }],
+    /**
+     * Disable server-based image optimization. Next.js does not support
+     * dynamic features with static exports.
+     *
+     * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+     */
+    unoptimized: true,
   },
+  /** Deployment */
+  output: 'export',
+  basePath: 'portfolio',
 }
 
 module.exports = nextConfig
